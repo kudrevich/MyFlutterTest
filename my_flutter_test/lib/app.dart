b96971161565f2fe7ded11dce26df6b7e99a7f99
kudrevich <ivan.kudrevich@yandex.ru>
2019-11-16 18:52:18 +0300

    Changed everything

---
+++ my_flutter_test/lib/app.dart
@@ -0,0 +1,13 @@
+// app.dart
+
+import 'package:flutter/material.dart';
+import 'package:my_flutter_test/location_detail/location_detail.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: LocationDetail(),
+    );
+  }
+}
